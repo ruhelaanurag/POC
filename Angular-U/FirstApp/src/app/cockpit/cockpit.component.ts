7b4f86771dd4253a13d7af5e18eb477cd4c28d56
Anurag Ruhela <Anurag.Ruhela@thomsonreuters.com>
2022-12-28 00:56:25 +0530

    Added cockpit and serverElement component.

---
+++ Angular-U/FirstApp/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.scss']
+})
+export class CockpitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  serverAdded(){
+    alert('server Added');
+  }
+
+  bluePrintAdded(){
+    alert('blueprint added');
+  }
+
+}
