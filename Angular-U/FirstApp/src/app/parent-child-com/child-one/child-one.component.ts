7575fb5e9a078b6d9a840fe732864a5ee5e6a7f2
Anurag Ruhela <Anurag.Ruhela@thomsonreuters.com>
2023-01-12 16:06:46 +0530

    removed extra files. added template to ts

--- Angular-U/FirstApp/src/app/parent-child-com/child-one/child-one.component.ts
+++ Angular-U/FirstApp/src/app/parent-child-com/child-one/child-one.component.ts
@@ -2,8 +2,15 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child-one',
-  templateUrl: './child-one.component.html',
-  styleUrls: ['./child-one.component.scss']
+  template: `
+<div style="border:solid 1px blue; max-width:200px; min-height:150px">
+  child
+  <h2>This is the value {{getData}}</h2>
+  <br /><br /><br />
+  <input type="text" name="txtInputChild" #txtChild value="" /><br />
+  <button (click)="buttonClicked(txtChild.value)">Click Me!</button>
+</div>
+`
 })
 export class ChildOneComponent implements OnInit {
   @Input() getData = '';
--- Angular-U/FirstApp/src/app/parent-child-com/parent-one/parent-one.component.ts
+++ Angular-U/FirstApp/src/app/parent-child-com/parent-one/parent-one.component.ts
@@ -2,8 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-parent-one',
-  templateUrl: './parent-one.component.html',
-  styleUrls: ['./parent-one.component.scss']
+  template:`
+  <div style="border:solid 1px black; max-width:200px; min-height:150px">
+    parent component
+    <input type="text" name="getValue" [value]="[(itemValue)]" /><br />
+    {{itemValue}} <br /><br />
+    <app-child-one [getData]="itemValue" (setData)="setItemValue($event)"></app-child-one>
+  </div>
+`
 })
 export class ParentOneComponent implements OnInit {
   public itemValue: string = 'default';
