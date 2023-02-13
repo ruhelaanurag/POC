5f015f33c43b647dad7f932fa3dea1fbb098d4fa
Anurag Ruhela <anuragruhela.net@gmail.com>
2023-02-13 15:49:10 +0530

    Update bookmark.js

--- Favlet/bookmark.js
+++ Favlet/bookmark.js
@@ -1,5 +1,6 @@
 javascript: (function() {
     let loc = window.location.href;
+    let desc = prompt('bookmark description');
     let url = 'https://docs.google.com/forms/d/e/1FAIpQLSdXmXJifBAs8j_ihngJ0YRufe3rc19X0v3E9Ds39kRrfX0okg/formResponse?&submit=Submit?usp=pp_url&entry.257107612=';
     fetch(url + loc)
 })();
