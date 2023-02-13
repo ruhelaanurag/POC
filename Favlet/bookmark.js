0e984a37eb78539418dd2fcc8982e69619b29fdc
Anurag Ruhela <anuragruhela.net@gmail.com>
2023-02-13 15:37:00 +0530

    Create bookmark.js

---
+++ Favlet/bookmark.js
@@ -0,0 +1,5 @@
+javascript: (function() {
+    let loc = window.location.href;
+    let url = 'https://docs.google.com/forms/d/e/1FAIpQLSdXmXJifBAs8j_ihngJ0YRufe3rc19X0v3E9Ds39kRrfX0okg/formResponse?&submit=Submit?usp=pp_url&entry.257107612=';
+    fetch(url + loc)
+})();
