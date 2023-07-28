128a472fa391cd2d25e8cd628e16f195812bb8fb
Anurag Ruhela <anuragruhela.net@gmail.com>
2023-07-28 19:07:59 +0530

    Create stackexchangeLogin.js

---
+++ Favlet/stackexchangeLogin.js
@@ -0,0 +1,8 @@
+javascript: (function () {
+   document.getElementById('email').value = 'email@email.com';
+   document.getElementById('password').value = 'password';
+   document.getElementById('submit-button').click();
+})();
+
+
+/*javascript:(function(){document.getElementById('email').value = 'email@email.com';document.getElementById('password').value = 'password';document.getElementById('submit-button').click();})();*/
