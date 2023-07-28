dd354f18d563607eed1f6c593e4e045c280d37f7
Anurag Ruhela <anuragruhela.net@gmail.com>
2023-07-28 19:09:40 +0530

    Create gfgLogin.js

---
+++ Favlet/gfgLogin.js
@@ -0,0 +1,9 @@
+javascript: (function () {
+   document.getElementsByClassName('header-main__signup login-modal-btn')[0].click();
+   document.getElementById('luser').value = 'username';
+   document.getElementById('password').value = 'password';
+   document.getElementsByClassName('btn btn-green signin-button')[0].click();
+})();
+
+
+/*javascript:(function(){document.getElementsByClassName('header-main__signup login-modal-btn')[0].click();document.getElementById('luser').value = 'username';document.getElementById('password').value = 'password';document.getElementsByClassName('btn btn-green signin-button')[0].click();})();*/
